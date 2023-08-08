1b562cbcf908580599e240d8e2fa7462609a1cbd
sanjukta97 <sanjukta@codilar.com>
2023-08-08 23:42:41 +0530

    configuration add

--- app/code/MantraMakers/OmniTranslate/Block/OmniTranslateData.php
+++ app/code/MantraMakers/OmniTranslate/Block/OmniTranslateData.php
@@ -5,6 +5,7 @@ namespace MantraMakers\OmniTranslate\Block;
 use Magento\Framework\View\Element\Template;
 use MantraMakers\OmniTranslate\Model\ResourceModel\TranslateModel\Collection;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class OmniTranslateData extends Template
 {
@@ -17,27 +18,48 @@ class OmniTranslateData extends Template
      * @var StoreManagerInterface
      */
     private StoreManagerInterface $storeManager;
+    /**
+     * @var ScopeConfigInterface
+     */
+    private ScopeConfigInterface $scopeConfig;
 
     public function __construct(
         Template\Context $context,
         Collection $translateModel,
         StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     )
     {
         parent::__construct($context, $data);
         $this->translateModel = $translateModel;
         $this->storeManager = $storeManager;
+        $this->scopeConfig = $scopeConfig;
     }
 
-
+    /**
+     * @return array|null
+     */
     public function getTranslateData()
     {
-        return $this->translateModel->addFilter('store_id', $this->getCurrentStoreId() )->getData();
+        if ($this->getEnableModule) {
+            return $this->translateModel->addFilter('store_id', $this->getCurrentStoreId())->getData();
+        } else {
+            return null;
+        }
     }
 
+    /**
+     * @return int
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function getCurrentStoreId()
     {
         return $this->storeManager->getStore()->getId();
+    }
+
+    public function getEnableModule() {
+        return $this->scopeConfig->getValue("mantramakers_omnitranslate/general/enabled",
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,$this->storeManager->getStore()->getStoreId();
     }
 }
